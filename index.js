c240588a922026f1ad213eb3bf0374028aea3909
Dalia Cavazos <dalia.codes@gmail.com>
2021-04-01 23:22:01 -0400

    git-r-done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "The Muffin Man",
+    address: "Drury Lane"
+}
+
+function updateEmployeeWithKeyAndValue(employee,name,address) {
+    const newEmployee = {...employee};
+    newEmployee[name]=address;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,address) {
+    employee[name]=address;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,name) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
